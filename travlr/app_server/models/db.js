bc14886eee19b1800f56fa8368fd73a59be7aafb
masonutt99 <65191595+masonutt99@users.noreply.github.com>
2023-01-29 16:46:59 -0500

    mod4

---
+++ travlr/app_server/models/db.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURI = 'mongodb://${host}/travlr';
+const readLine = require('readline');
+
+//avoid 'current Server Discovery and Monitoring engine is deprecated'
+mongoose.set('useUnifiedTopology', true);
+
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }), 1000);
+}
+
+mongoose.connection.on('connected', () => {});
+
+mongoose.connection.on('error', () => {});
+
+mongoose.connection.on('disconnnected', () => {});
+
+mongoose.connection.on('win32', () => {});
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close(  () => {
+        console.log('Mongoose disconnectec through ${msg}');
+        callback();
+    });
+};
+
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', () =>{
+    gracefulShutdown('Heroku app shutdown', () =>{
+        process.exit(0);
+    });
+});
+
+connect();
+
+//bring in hte Mongoose schema
+require('./travlr');

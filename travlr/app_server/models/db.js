c979084696b399af25b9c217a6feb243fbcc6022
masonutt99 <masonutt99@gmail.com>
2023-01-29 23:18:12 -0500

    m

--- travlr/app_server/models/db.js
+++ travlr/app_server/models/db.js
@@ -22,28 +22,28 @@ mongoose.connection.on('disconnnected', () => {});
 mongoose.connection.on('win32', () => {});
 
 const gracefulShutdown = (msg, callback) => {
-    mongoose.connection.close(  () => {
-        console.log('Mongoose disconnectec through ${msg}');
-        callback();
-    });
+    // mongoose.connection.close(  () => {
+    //     console.log('Mongoose disconnectec through ${msg}');
+    //     callback();
+    // });
 };
 
 process.once('SIGUSR2', () => {
-    gracefulShutdown('nodemon restart', () => {
-        process.kill(process.pid, 'SIGUSR2');
-    });
+    // gracefulShutdown('nodemon restart', () => {
+    //     process.kill(process.pid, 'SIGUSR2');
+    // });
 });
 
 process.on('SIGINT', () => {
-    gracefulShutdown('app termination', () => {
-        process.exit(0);
-    });
+    // gracefulShutdown('app termination', () => {
+    //     process.exit(0);
+    // });
 });
 
 process.on('SIGTERM', () =>{
-    gracefulShutdown('Heroku app shutdown', () =>{
-        process.exit(0);
-    });
+    // gracefulShutdown('Heroku app shutdown', () =>{
+    //     process.exit(0);
+    // });
 });
 
 connect();
